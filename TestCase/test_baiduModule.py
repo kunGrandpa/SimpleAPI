f3f8c24cb8a83555814c2ce785fd4c58296e1bd8
kun <653807699@qq.com>
2022-08-08 10:44:59 +0800

    更新请求数据

--- TestCase/test_baiduModule.py
+++ TestCase/test_baiduModule.py
@@ -6,6 +6,7 @@
 
 import pytest
 import allure
+from requests import request
 
 
 # 标记模块名称，最后会在报告中显示
@@ -13,8 +14,7 @@ import allure
 class TestBaidu:
 
     @allure.story("百度请求")  # 用例名称，可以直接写接口名称
-    @allure.title("")  # 用例标题
-    @pytest.mark.parametrize()
-    def test_case_1(self):
-
+    @allure.title("{title}")  # 用例标题
+    @pytest.mark.parametrize("method, url, title")
+    def test_case_1(self, method, url, title):
         pass
---
+++ Utils/dataUtils/setRequestData.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @time: 2022-08-08 10:44:21
+# @author: 鹰眼测试
